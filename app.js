4c4fce114e628455f01b892369ca309ca217f8e6
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-19 23:06:44 +0530

    html and css code added

---
+++ app.js
@@ -0,0 +1,7 @@
+const dateOfBirth = document.querySelector("#birthday");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", function checkLuckyNumber()   {
+    console.log(luckyNumber);
+});
